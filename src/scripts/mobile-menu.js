e16168e2306b491fe1eb64fa237efa6d69038986
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2025-06-18 15:32:24 +0300

    Add header animations #5

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -1,37 +1,40 @@
-const toggleButton = document.getElementById("header-btn");
-const mobileMenu = document.getElementById("mobile-menu");
-const menuLinks = mobileMenu.querySelectorAll("a");
+if (window.matchMedia("(max-width: 47.999rem)").matches) {
+  const toggleButton = document.getElementById("header-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const menuLinks = mobileMenu.querySelectorAll("a");
 
-const openMenu = () => {
-  mobileMenu.style.display = "flex";
-  document.body.style.overflowY = "hidden";
+  const openMenu = () => {
+    mobileMenu.style.display = "flex";
+    document.body.style.overflowY = "hidden";
 
-  requestAnimationFrame(() => {
-    toggleButton.setAttribute("aria-expanded", "true");
-  });
-};
+    requestAnimationFrame(() => {
+      toggleButton.setAttribute("aria-expanded", "true");
+    });
+  };
 
-const closeMenu = () => {
-  mobileMenu.classList.remove("mob-menu_active");
-  toggleButton.setAttribute("aria-expanded", "false");
-  document.body.style.overflowY = "";
-
-  setTimeout(() => {
-    mobileMenu.style.display = "none";
-  }, 250);
-};
-
-toggleButton.addEventListener("click", () => {
-  const isOpen = toggleButton.getAttribute("aria-expanded") === "true";
-  isOpen ? closeMenu() : openMenu();
-});
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    closeMenu();
+  const closeMenu = () => {
+    mobileMenu.classList.remove("mob-menu_active");
+    toggleButton.setAttribute("aria-expanded", "false");
+    document.body.style.overflowY = "";
 
     setTimeout(() => {
-      history.replaceState(null, "", window.location.pathname);
-    }, 10);
+      mobileMenu.style.display = "none";
+    }, 250);
+  };
+
+  toggleButton.addEventListener("click", () => {
+    const isOpen = toggleButton.getAttribute("aria-expanded") === "true";
+    isOpen ? closeMenu() : openMenu();
   });
-});
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      closeMenu();
+
+      setTimeout(() => {
+        history.replaceState(null, "", window.location.pathname);
+      }, 10);
+    });
+  });
+}
+export {};
